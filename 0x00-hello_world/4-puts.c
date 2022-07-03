c2263d1bb9dabc02146048b71924a5a72e9fc1d4
Modibbo96 <modibboel@gmail.com>
2022-07-03 15:53:26 -0700

    C programming for command puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+/*
+ * main - entry point in C
+ * return 0 - for int type function return integer
+ */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
