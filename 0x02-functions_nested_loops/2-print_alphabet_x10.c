8c0dbd7357e2f9a5348685b1d9f02e150b8fc42c
Modibbo96 <modibboel@gmail.com>
2022-07-05 14:43:50 -0700

    Task 2 print alphabet 1o

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+while (i < 10)
+{
+c = 'a';
+while (c <= 'z')
+{
+_putchar('\n');
+i++;
+}
+}
+}
