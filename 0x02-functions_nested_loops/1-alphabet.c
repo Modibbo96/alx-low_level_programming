274fe57b6d2ef1970352402d969b171aef1598f0
Modibbo96 <modibboel@gmail.com>
2022-07-05 14:20:00 -0700

    Task 1. print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: Always return 0
+ */
+void print_alphabet(void);
+{
+int x;
+for (x = '97'; x <= '122'; x++)
+{
+_putchar (x);
+}
+_putchar('\n');
+}
