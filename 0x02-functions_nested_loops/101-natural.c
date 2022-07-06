f34dbdc6570b8d8374d7252a01e656b994cd02d1
Modibbo96 <modibboel@gmail.com>
2022-07-06 14:48:48 -0700

    correction natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 unsigned long int sum3, sum5, sum;
-int i
-sum3 = 0;
-sum5 = 0;
-sum = 0;
+int i;
+int sum3 = 0;
+int sum5 = 0;
+int sum = 0;
 for (i = 0; i < 1024; ++i)
 {
 if ((i % 3) == 0)
