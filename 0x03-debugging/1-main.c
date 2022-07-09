f6098f842eee069af0a82fd0b2ec7c1fdfdf5eb8
Modibbo96 <modibboel@gmail.com>
2022-07-08 17:58:59 -0700

    debugging is a big bug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,13 +9,14 @@ int main(void)
 {
 int i;
 
-printf("Infinite loop incoming :(\n");
+printf("Infinite loop is coming :(\n");
 i = 0;
+
 
 /**
  * while (i < 10)
  * {
- *  putchar(i);
+ * putchar(i);
  * }
  */
 
