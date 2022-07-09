053e37a1f6b46f36ea30650cc09c10bcfc78f8f1
Modibbo96 <modibboel@gmail.com>
2022-07-08 19:12:47 -0700

    task 07 done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a straight line
+ *
+ *  @n: number of lines
+ */
+
+void print_diagonal(int n)
+{
+int 1, x;
+for (l = 0; l <= n; l++)
+{
+for (x = 1; x <= l; x++)
+if  (l == x)
+{
+_putchar('\\');
+_putchar('\n');
+}
+else
+{
+_putchar(' ');
+}
+}
+if (n <= 0)
+{
+_putchar('\n');
+}
+}
