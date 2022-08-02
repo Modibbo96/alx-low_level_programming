609cfb539688d505a7de8ec2804c9cfe5f0b4648
Modibbo96 <modibboel@gmail.com>
2022-08-02 05:18:31 -0700

    checking betty

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,14 +15,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i = 0;
-
-  while (i < n)
-    {
-      s[i] = b;
-      i++;
-    }
-  return (s);
+	unsigned int i = 0;
+	while (i < n)
+	{
+	    s[i] = b;
+	    i++;
+	}
+	return (s);
 }
 
 /**
@@ -37,16 +36,21 @@ char *_memset(char *s, char b, unsigned int n)
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  char *ptr;
+	char *ptr;
 
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  ptr = malloc(nmemb * size);
-  if (ptr == NULL)
-    return (NULL);
-  _memset(ptr, 0, nmemb * size);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-  return (ptr);
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+
+	return (ptr);
 }
 
 
